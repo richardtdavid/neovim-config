5e9647420f9f4406d8144c30924a2600043042b1
Richard David <driichard@gmail.com>
2022-08-02 22:42:35 -0500

    nvim tree toggle command

--- lua/Rdavid/plugins/nvim-tree.lua
+++ lua/Rdavid/plugins/nvim-tree.lua
@@ -1,21 +1,26 @@
 local function init()
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
+  require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    view = {
+      adaptive_size = true,
+      mappings = {
+        list = {
+          { key = "u", action = "dir_up" },
+        },
       },
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+  })
+
+  local map = vim.api.nvim_set_keymap
+  local options = { noremap = true }
+
+  map('n', '<leader>ntt', "<cmd>NvimTreeToggle<cr>", options)
 end
 
 return {
